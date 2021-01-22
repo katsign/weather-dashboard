dda5dd1a18b3787c8b6361ffffa9ed8259a14074
Kat Buchanan <katrbuch@gmail.com>
2021-01-21 21:07:37 -0600

    html structure, utilities

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/*
+
+// UTILITIES: 
+    API key weatherapp = "6ee75339838f57228cbda7f75505c443"
+    API key Default= "f9994fb5e34a2dcdfcbb911a1360f838"
+
+// Elements tracked:
+    <button id="search">
+    <div id="resultCard">
+    <div id="populateResult">
+
+*/
