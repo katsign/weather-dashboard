e18aa8c7ab2783adac37547e208d62b7425762fb
Kat Buchanan <katrbuch@gmail.com>
2021-01-21 22:17:06 -0600

    search triggers api call, logs return

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 /*
-
-// UTILITIES: 
+// UTILITIES:
     API key weatherapp = "6ee75339838f57228cbda7f75505c443"
     API key Default= "f9994fb5e34a2dcdfcbb911a1360f838"
 
@@ -10,3 +9,32 @@
     <div id="populateResult">
 
 */
+
+$(document).ready(function () {
+
+    function getResult(event) {
+        event.preventDefault();
+
+        $('#populateResult').empty();
+
+        var searchCity = $('input').val();
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=6ee75339838f57228cbda7f75505c443",
+            type: "GET",
+        }).done(function(data){
+            console.log(data);
+        })
+        /*
+            .then(function (response) {
+                console.log(response);
+
+                for (let i = 0; i < response.results.length; i++) {
+                }
+
+
+            }) */
+    }
+
+    $('#search').on('click', getResult)
+
+});
