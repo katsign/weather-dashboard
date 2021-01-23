3c021631764ea29b017e4f52aac29d59e01f0f18
Kat Buchanan <katrbuch@gmail.com>
2021-01-22 21:15:34 -0600

    call populates card, added html elements for build

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 /*
+
 // UTILITIES:
     API key weatherapp = "6ee75339838f57228cbda7f75505c443"
     API key Default= "f9994fb5e34a2dcdfcbb911a1360f838"
 
+// For eventually calling with lon/lat data from searchCity return:
+    https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+    coord.lon
+    coord.lat
+
 // Elements tracked:
-    <button id="search">
-    <div id="resultCard">
-    <div id="populateResult">
+    <button id="search"> | #search
+    <div id="resultCard"> | #resultCard
+    <div id="populateResult"> | #populateResult
 
 */
 
@@ -18,21 +24,38 @@ $(document).ready(function () {
         $('#populateResult').empty();
 
         var searchCity = $('input').val();
+        
+
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=6ee75339838f57228cbda7f75505c443",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&appid=6ee75339838f57228cbda7f75505c443",
             type: "GET",
-        }).done(function(data){
-            console.log(data);
-        })
-        /*
-            .then(function (response) {
+        }).then(function (response) {
                 console.log(response);
 
-                for (let i = 0; i < response.results.length; i++) {
-                }
+                var results = response;
+                var name = results.name;
+                var temperature = results.main.temp;
+                var humidity = results.main.humidity;
+                var windSpeed = results.wind.speed;
+                var date = moment().format('L');
 
 
-            }) */
+                $('<h3>').attr('id', 'cardTitle').text(name).appendTo('#populateResult');
+                $('<hr>').appendTo('#populateResult');
+                $('<p>').attr('class', 'cardData').text('Temperature: ' + temperature + '°F').appendTo('#populateResult');
+                $('<p>').attr('class', 'cardData').text('Humidity: ' + humidity + '%').appendTo('#populateResult');
+                $('<p>').attr('class', 'cardData').text('Wind Speed: ' + windSpeed + 'mph').appendTo('#populateResult');
+
+
+
+                var lat = response.coord.lat;
+                var lon = response.coord.lon;
+
+                
+                  
+
+            })
+
     }
 
     $('#search').on('click', getResult)
